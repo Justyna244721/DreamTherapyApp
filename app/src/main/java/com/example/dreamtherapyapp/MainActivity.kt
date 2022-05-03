5849334fa42143354727fc51709dd3859ea9c2b9
Justyna244721 <244721@student.pwr.edu.pl>
2022-05-03 13:26:21 +0200

    widoki 4. Przelaczanie stron

--- app/src/main/java/com/example/dreamtherapyapp/MainActivity.kt
+++ app/src/main/java/com/example/dreamtherapyapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.dreamtherapyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var LogIn: Button?=null
+    private var Signin: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogIn = findViewById(R.id.LogIn)
+        Signin= findViewById(R.id.SignIn)
+
+        //przycisk do strony Menu
+        LogIn?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openMenu()
+            }
+        })
+
+        //przycisk do strony z Rejestracją
+        Signin?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openSignIn()
+            }
+        })
+    }
+    //Przejście do strony Głównej
+    private fun openMenu(){
+        val intent= Intent(this,Menu::class.java)
+        startActivity(intent)
+    }
+    //Przejscie do strony z Rejestracją
+    private fun openSignIn(){
+        val intent= Intent(this,SignIn::class.java)
+        startActivity(intent)
     }
 }
