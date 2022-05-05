967e19c383669a47cf529f5e098c5b0005897b8c
Justyna244721 <244721@student.pwr.edu.pl>
2022-05-05 13:56:00 +0200

    validateRegisterDetails building

--- app/src/main/java/com/example/dreamtherapyapp/SignIn.kt
+++ app/src/main/java/com/example/dreamtherapyapp/SignIn.kt
@@ -3,12 +3,22 @@ package com.example.dreamtherapyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
-class SignIn : AppCompatActivity() {
+class SignIn : InformacjaRejestracyjna(){
 
     private var btnSaveSignIn : Button ?= null
+    private var edTvNameSignIn: EditText?=null
+    private var edTvSurnameSignIn: EditText?=null
+    private var edTvEmailSignIn: EditText?=null
+    private var edTvPasswordSignIn: EditText?=null
+    private var edTvReapetPasswordSignIn: EditText?=null
+    private var edTvAccessCodeSignIn: EditText?=null
+    private var edTvAgeSignIn: EditText?=null
+    private var accessCode : String ?= "1234"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +27,15 @@ class SignIn : AppCompatActivity() {
         btnSaveSignIn?.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 openMainActivity()
+                edTvNameSignIn=findViewById(R.id.edTvNameSignIn)
+                edTvSurnameSignIn=findViewById(R.id.edTvSurnameSignIn)
+                edTvEmailSignIn=findViewById(R.id.edTvEmailSignIn)
+                edTvPasswordSignIn=findViewById(R.id.edTvPasswordSignIn)
+                edTvReapetPasswordSignIn=findViewById(R.id.edTvReapetPasswordSignIn)
+                edTvAccessCodeSignIn=findViewById(R.id.edTvAccessCodeSignIn)
+                edTvAgeSignIn=findViewById(R.id.edTvAgeSignIn)
+
+
             }
         })
     }
@@ -24,5 +43,49 @@ class SignIn : AppCompatActivity() {
     private fun openMainActivity(){
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+    private fun validateRegisterDetails(): Boolean {
+        return when{
+            TextUtils.isEmpty(edTvNameSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_name),true)
+                false
+            }
+            TextUtils.isEmpty(edTvSurnameSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_surname),true)
+                false
+            }
+            TextUtils.isEmpty(edTvEmailSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_emal),true)
+                false
+            }
+            TextUtils.isEmpty(edTvPasswordSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password),true)
+                false
+            }
+            TextUtils.isEmpty(edTvReapetPasswordSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_reppassword),true)
+                false
+            }
+            edTvPasswordSignIn?.text.toString().trim {it <= ' '} != edTvReapetPasswordSignIn?.text.toString().trim{it <= ' '} -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_mismatch),true)
+                false
+            }
+            TextUtils.isEmpty(edTvAccessCodeSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_access_code),true)
+                false
+            }
+            edTvAccessCodeSignIn?.text.toString().trim {it <= ' '} != accessCode?.trim{it <= ' '} -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_mismatch_access_code),true)
+                false
+            }
+            TextUtils.isEmpty(edTvAgeSignIn?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_age),true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid",false)
+                true
+            }
+        }
     }
 }
