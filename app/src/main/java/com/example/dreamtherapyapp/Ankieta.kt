005203fc8a2df076af4a11424db20fd910a10382
Staszek <244896@student.pwr.edu.pl>
2022-05-18 09:12:42 +0200

    Ankieta questions development

--- app/src/main/java/com/example/dreamtherapyapp/Ankieta.kt
+++ app/src/main/java/com/example/dreamtherapyapp/Ankieta.kt
@@ -2,10 +2,33 @@ package com.example.dreamtherapyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class Ankieta : AppCompatActivity() {
+
+    private var ratingBarAnkieta : RatingBar?= null
+    private var cBoxYesAnkietaQ2: CheckBox?=null
+    private var cBoxNoAnkietaQ2: CheckBox?=null
+    private var cBoxYesAnkietaQ3: CheckBox?=null
+    private var cBoxNoAnkietaQ3: CheckBox?=null
+    private var switch1Tired: Switch?=null
+    private var switch2Frustreated: Switch?=null
+    private var switch3ItWasAllRight: Switch?=null
+    private var switch4FullOfEnergy: Switch?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ankieta)
+
+        ratingBarAnkieta = findViewById(R.id.ratingBarAnkieta)
+        cBoxYesAnkietaQ2=findViewById(R.id.cBoxYesAnkietaQ2)
+        cBoxNoAnkietaQ2=findViewById(R.id.cBoxNoAnkietaQ2)
+        cBoxYesAnkietaQ3=findViewById(R.id.cBoxYesAnkietaQ3)
+        cBoxNoAnkietaQ3=findViewById(R.id.cBoxNoAnkietaQ3)
+        switch1Tired=findViewById(R.id.switch1Tired)
+        switch2Frustreated=findViewById(R.id.switch2Frustreated)
+        switch3ItWasAllRight=findViewById(R.id.switch3ItWasAllRight)
+        switch4FullOfEnergy=findViewById(R.id.switch4FullOfEnergy)
     }
 }
